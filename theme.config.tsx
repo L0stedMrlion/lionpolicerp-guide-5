5afb8b213bd479bcadc42968cb6b4738e7df1e88
Mrlion <mrlion4202@gmail.com>
2025-03-22 13:29:18 +0000

    changes

--- theme.config.tsx
+++ theme.config.tsx
@@ -1,17 +1,74 @@
 import React from "react";
-import { DocsThemeConfig } from "nextra-theme-docs";
+import { DocsThemeConfig, useConfig } from "nextra-theme-docs";
+import { useRouter } from "next/router";
+import Head from "next/head";
+
+function useNextSeoProps() {
+  const { asPath } = useRouter();
+  const arr = asPath.replace(/[-_]/g, " ").split("/");
+  const category = arr[1] && arr[1][0] !== "#" ? arr[1] : "Lion Police RP";
+  const rawTitle = arr[arr.length - 1] || "Home";
+  const title =
+    /[a-z]/.test(rawTitle) && /[A-Z]/.test(rawTitle) ? rawTitle : "%s";
+
+  return {
+    titleTemplate: `${title} - ${
+      rawTitle === category
+        ? "Documentation"
+        : category.replace(/(^\w|\s\w)/g, (m) => m.toUpperCase())
+    }`,
+  };
+}
+
+function useHead() {
+  const { asPath } = useRouter();
+  const { frontMatter, title } = useConfig();
+  const description =
+    frontMatter.description || "Documentation Lion Police Roleplay";
+  return (
+    <Head>
+      <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+      <link rel="icon" type="image/png" href="/lion.ico" />
+      <link rel="shortcut icon" href="/lion.ico" />
+      <meta httpEquiv="Content-Language" content="en" />
+      <meta name="description" content={description} />
+      <meta name="og:title" content={title} />
+      <meta name="og:description" content={description} />
+    </Head>
+  );
+}
 
 const config: DocsThemeConfig = {
-  logo: <span>🦁 Lion Police RP</span>,
+  navigation: {
+    prev: false,
+    next: false,
+  },
+  primaryHue: 52,
+  logo: (
+    <div
+      style={{
+        paddingLeft: "50px",
+        lineHeight: "38px",
+        background:
+          "url('https://i.ibb.co/sszfTrX/policelogo.png') no-repeat left",
+        backgroundSize: "38px",
+        fontWeight: 550,
+      }}
+    >
+      🦁 Lion Police Roleplay
+    </div>
+  ),
   project: {
-    link: "https://github.com/shuding/nextra-docs-template",
+    link: "https://github.com/L0stedMrlion/lionpolicerp-guide-5",
   },
   chat: {
-    link: "https://discord.gg/rrZ7RpEUkb",
+    link: "https://discord.gg/Pd4FqRQ7aH",
   },
-  docsRepositoryBase: "https://github.com/shuding/nextra-docs-template",
+  docsRepositoryBase: "https://github.com/L0stedMrlion/lionpolicerp-guide-5",
+  useNextSeoProps: useNextSeoProps,
+  head: useHead,
   footer: {
-    text: "Nextra Docs Template",
+    text: "🦁 Lion Police Roleplay",
   },
 };
 
